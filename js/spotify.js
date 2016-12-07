0f46a417866d907473e8588383ea9ad254e1881c
Ryo Mashima <ma4ryo@gmail.com>
2016-12-07 17:49:24 +0900

    Search Result empty

--- js/spotify.js
+++ js/spotify.js
@@ -1,6 +1,8 @@
 // find template and compile it
 var templateSource = document.getElementById('results-template').innerHTML,
     template = Handlebars.compile(templateSource),
+    notfound_templateSource = document.getElementById('notfound-template').innerHTML,
+    notfound_template = Handlebars.compile(notfound_templateSource),
     resultsPlaceholder = document.getElementById('results'),
     playingCssClass = 'playing',
     audioObject = null;
@@ -19,10 +21,15 @@ var searchAlbums = function (query) {
         url: 'https://api.spotify.com/v1/search',
         data: {
             q: query,
-            type: 'album'
+            type: 'album',
+            limit: 30,
         },
         success: function (response) {
-            resultsPlaceholder.innerHTML = template(response);
+            if (response.albums.total > 0) {
+                resultsPlaceholder.innerHTML = template(response);
+            } else {
+                resultsPlaceholder.innerHTML = notfound_template(response);
+            }
         }
     });
 };
